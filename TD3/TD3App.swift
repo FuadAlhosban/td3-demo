f205cd90a7814885eb6fa34aea172ec9b07fa352
Dr Fuad Alhosban <falhosban@hct.ac.ae>
2023-07-31 03:08:33 +0400

    Initial Commit

---
+++ TD3/TD3App.swift
@@ -0,0 +1,17 @@
+//
+//  TD3App.swift
+//  TD3
+//
+//  Created by Fuad Alhosban on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TD3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
